d45b054f291e457ee6e9bf0bb727809f56b9d39f
João Paulo <paulosantiago.cavalcante@gmail.com>
2022-06-25 18:40:39 -0300

    Update com sucesso

--- app/admin.py
+++ app/admin.py
@@ -4,5 +4,3 @@ from app.models import Postagem
 @admin.register(Postagem)
 class Postagem(admin.ModelAdmin):
     list_display = ['titulo', 'conteudo', 'autor', 'criado_em']
-
-# admin.site.register(Postagem)
