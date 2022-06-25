46b3440e4e72f4d4a6c228ac9f23840a90588f7b
João Paulo <paulosantiago.cavalcante@gmail.com>
2022-06-25 18:39:14 -0300

    Alterado com sucesso

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from app.models import Postagem
 
+@admin.register(Postagem)
+class Postagem(admin.ModelAdmin):
+    list_display = ['titulo', 'conteudo', 'autor', 'criado_em']
 
-admin.site.register(Postagem)
+# admin.site.register(Postagem)
